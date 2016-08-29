2546dd3882f747fde8b9b202e36913780f4c00f0
dsa <admin@petermolgaard.com>
2016-08-29 10:46:55 +0700

    config fixed to work with app's as default

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@
 
 module.exports = {
     timeout: 15000,
-    sourceDir: '../nx-sails-assets-sync-demo/node_modules',
-    targetDir: '../nx-sails-assets-sync-demo/assets',
-    tasksDir: '../nx-sails-assets-sync-demo/tasks'
+    sourceDir: 'node_modules',
+    targetDir: 'assets',
+    tasksDir: 'tasks'
 };
